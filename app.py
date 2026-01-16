79e9a8dcde781027a2e5093381db453c496e5bec
Samuel <adeoyesamuel100@gmail.com>
2026-01-16 03:15:06 +0100

    update requirements.txt file

--- app.py
+++ app.py
@@ -60,6 +60,8 @@ def index():
 
     return render_template('index.html', prediction=prediction_result, preview=preview_html)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    app.run(host="0.0.0.0", port=port, debug=False)
 
