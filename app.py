e5d9df1f08d35cde66cbd7fc3bf65084b3d03080
Samuel <adeoyesamuel100@gmail.com>
2026-01-22 00:22:28 +0100

    update files

--- app.py
+++ app.py
@@ -37,4 +37,6 @@ def home():
                            neighborhoods=neighborhoods)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    print("Starting Flask server...")
+    print("Open this link in your browser: http://127.0.0.1:5000")
+    app.run(host="0.0.0.0", port=5000, debug=True)
